de7ff47278f436dde6e5ac60fb626d0c690d3acb
Elbis <m.kutayasan@gmail.com>
2018-10-09 18:01:54 +0300

    Update splash.js

--- splash.js
+++ splash.js
@@ -1,48 +1,72 @@
 import React from 'react';
-import { StyleSheet, Platform, Image, Text, View, ScrollView, AsyncStorage } from 'react-native';
+import { StyleSheet, Platform, Image, Text, View, ScrollView, AsyncStorage, ImageBackground } from 'react-native';
 import firebase from 'react-native-firebase';
 export default class splash extends React.Component {
     static navigationOptions = {
-        header:null,
-      };
+        header: null,
+    };
     constructor() {
-      super();
-      this.state = {
-        // firebase things?
-      };
+        super();
+        this.state = {
+            // firebase things?
+            timePassed: false,
+        };
     }
-    componentDidMount(){
-      
-      const launched = AsyncStorage.getItem("launched")
-    .then(value => {
-      if (value==null){
-        AsyncStorage.setItem("launched",'true')
-        this.props.navigation.navigate("verify")
-        
-      }
-      else {
-        this.props.navigation.navigate("webview")
-      }
-    })
+    componentDidMount() {
+
+
+        const launched = AsyncStorage.getItem("launched")
+            .then(value => {
+               
+                if (value == null) {
+                    setTimeout(() => {
+                        this.props.navigation.navigate("verify")
+
+                        //Put All Your Code Here, Which You Want To Execute After Some Delay Time.
+
+                    }, 2000);
+
+
+
+
+                }
+                else {
+                    setTimeout(() => {
+                        this.props.navigation.navigate("webview")
+                        //Put All Your Code Here, Which You Want To Execute After Some Delay Time.
+
+
+                    }, 2000);
+
+
+                }
+            })
     }
-   
-  
+
+
+
+
     render() {
-      return (
-        <ScrollView style={{flex:1}}>
-          <Text style={{fontSize:32, fontWeight:'bold', textAlign:'center', alignSelf:'center'}}> LOADING</Text>
-          
-        </ScrollView>
-      );
+        return (
+            <View style={styles.container}>
+
+                <ImageBackground source={require("C:/Users/asan/Desktop/RNFirebase/tedarik/react-native-firebase-starter/src/img/splash.jpg")} style={{width:'100%',height:'80%'}}>
+
+                    <Text style={{ fontSize: 32, fontWeight: 'bold', textAlign: 'center', alignSelf: 'center', color:'white' }}> YÜKLENİYOR</Text>
+                    <Text style={{ fontSize: 44, fontWeight: 'bold', textAlign: 'center', alignSelf: 'center', color:'white' }}> . . .</Text>
+
+                </ImageBackground>
+            </View>
+
+        );
     }
-  }
-  
-  const styles = StyleSheet.create({
+}
+
+const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#F5FCFF',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#753A8A',
     }
-  });
-  
+});
